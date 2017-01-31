9d4371909cba03bfc06657e8e7dbcfb64b301104
Emily Hough-Kovacs <emilyhk@spotify.com>
2017-01-31 00:00:36 -0500

    login view yay

--- Uprising/AppDelegate.swift
+++ Uprising/AppDelegate.swift
@@ -15,7 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let controllerId="login";
+        
+        let storyboard=UIStoryboard(name: "Main", bundle: nil)
+        let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: controllerId) as UIViewController
+        self.window?.rootViewController = initViewController
+        
         return true
     }
 
---
+++ Uprising/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Uprising
+//
+//  Created by Emily Hough-Kovacs on 1/30/17.
+//  Copyright © 2017 Emily Hough-Kovacs. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
